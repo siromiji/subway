3078e2a27dd3e08d6a8c58dc8ab4ebefab1b2577
siromiji <mnmn82018282@gmail.com>
2025-10-03 15:28:20 +0900

    map 페이지에서 리스트페이지로 이동하는 버그 삭제

--- src/components/MapPage.jsx
+++ src/components/MapPage.jsx
@@ -1,11 +1,10 @@
-import { useNavigate } from 'react-router-dom';
+
 import './MapPage.css';
 function MapPage(){
-    const navigate = useNavigate();
     return(
         <>
         <div className="MapPage-MapContainer">
-            <img src="/subwayroad.jfif" alt="지하철 노선 이미지" onClick={()=>{navigate('/subwaystationlist')}}/>
+            <img src="/subwayroad.jfif" alt="지하철 노선 이미지" />
 
         </div>
   
